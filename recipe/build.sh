9a6b6352dd6b2c9aeca9938e50f9be992e6caf72
Sam Gillingham <gillingham.sam@gmail.com>
2016-03-23 13:07:50 +1000

    made cmake arguments line up vertically [skip appveyor]

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,14 @@ else
     SONAME="so"
 fi
 
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX -D TIFF_LIBRARY=$PREFIX/lib/libtiff.${SONAME} -D TIFF_INCLUDE_DIR=$PREFIX/include -D PNG_LIBRARY_RELEASE=$PREFIX/lib/libpng.${SONAME} -D PNG_PNG_INCLUDE_DIR=$PREFIX/include -D ZLIB_LIBRARY=$PREFIX/lib/libz.${SONAME} -D ZLIB_INCLUDE_DIR=$PREFIX/include .
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D TIFF_LIBRARY=$PREFIX/lib/libtiff.${SONAME} \
+      -D TIFF_INCLUDE_DIR=$PREFIX/include \
+      -D PNG_LIBRARY_RELEASE=$PREFIX/lib/libpng.${SONAME} \
+      -D PNG_PNG_INCLUDE_DIR=$PREFIX/include \
+      -D ZLIB_LIBRARY=$PREFIX/lib/libz.${SONAME} \
+      -D ZLIB_INCLUDE_DIR=$PREFIX/include \
+      .
 
 make
 make tests
